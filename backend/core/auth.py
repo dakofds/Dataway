b9c09431bd8e7fcc438718973e08b01a7f6fd492
dakofds <156973927+dakofds@users.noreply.github.com>
2025-07-17 07:48:05 -0300

    Update auth.py

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -1,5 +1,6 @@
 from flask import session, redirect, url_for
 from functools import wraps
+from backend.models import db, User  
 
 def login_required(f):
     @wraps(f)
@@ -12,7 +13,9 @@ def login_required(f):
 def staff_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        name = session["username"]
+        name = session.get("username")
         verify = db.query(User).filter_by(name=name, is_staff=True).first()
         if not verify:
             return redirect(url_for('board.read_root', board='home'))
+        return f(*args, **kwargs) 
+    return decorated_function 
